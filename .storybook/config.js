a52a90787c3b77baf37d575469f454c2e527581b
Jeff Reiner <theundeadghost@gmail.com>
2019-02-07 18:37:09 +0100

    prod deploy!  (#3)
    * add gitignore
    
    * add project structure
    
    * :heavy_plus_sign: adding prettier and format npm command
    
    * :art: running prettier
    
    * :memo: adding Prettier badge
    
    * :construction_worker: adding netlify and enabling static export

---
+++ .storybook/config.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { addDecorator, configure } from "@storybook/react";
+import { withOptions } from "@storybook/addon-options";
+import {
+  configureViewport,
+  INITIAL_VIEWPORTS,
+  withViewport
+} from "@storybook/addon-viewport";
+
+const req = require.context("../packages", true, /.stories.js$/);
+
+function loadStories() {
+  console.log("Load Stories", req.keys());
+  req.keys().forEach(filename => req(filename));
+}
+
+configureViewport({
+  ...INITIAL_VIEWPORTS
+});
+
+addDecorator(
+  withOptions({
+    name: "Centrifuge Axis",
+    url: "https://axis.centrifuge.io",
+    sortStoriesByKind: true,
+    hierarchySeparator: /\//,
+    hierarchyRootSeparator: /\|/
+  })
+);
+
+addDecorator(withViewport());
+
+configure(loadStories, module);
---
+++ packages/theme/src/AxisTheme.js
@@ -0,0 +1,56 @@
+// Global Styles
+import React from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components/macro";
+import { Grommet, ThemeContext } from "grommet";
+import { axisTheme } from "./theme";
+
+const GlobalsStyles = createGlobalStyle`
+  * {
+    
+    &::-webkit-scrollbar {
+      width: 8px;
+    }
+     
+    &::-webkit-scrollbar-thumb {
+      background: ${axisTheme.global.colors["light-6"]};
+      border-radius: 20px;
+    }
+    
+    &::-webkit-scrollbar-track {
+      background: transparent;
+      border-radius: 20px;
+    }
+   
+  }
+  
+   body,html {
+    margin:0;
+    height:100%;
+    color: ${axisTheme.global.colors["light-1"]};
+    
+  }
+  a {
+    color: ${axisTheme.global.colors.brand};
+    text-decoration: none;
+    &:hover {
+        text-decoration: underline;
+    }
+  }
+  #app-root {
+    height:100%;
+  }
+`;
+
+export const AxisTheme = props => {
+  return (
+    <Grommet full={true} theme={props.theme || axisTheme}>
+      <GlobalsStyles />
+      <ThemeContext.Consumer>
+        {theme => {
+          // Make theme available for all styled components
+          return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
+        }}
+      </ThemeContext.Consumer>
+    </Grommet>
+  );
+};
