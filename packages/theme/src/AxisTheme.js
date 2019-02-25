52f6d11f165967816b40ed2d53657cd97e0381d5
Jeff Reiner <theundeadghost@gmail.com>
2019-02-25 17:12:55 +0100

    :bento: trying to add font assets

--- packages/theme/src/AxisTheme.js
+++ packages/theme/src/AxisTheme.js
@@ -4,10 +4,45 @@ import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { Grommet, ThemeContext } from "grommet";
 import { axisThemeConfig } from "./theme";
 
+// import AvenirRegular from "./fonts/721263/2cd55546-ec00-4af9-aeca-4a3cd186da53.woff2";
+// import AvenirMedium from "./fonts/721275/627fbb5a-3bae-4cd9-b617-2f923e29d55e.woff2";
+// import AvenirDemiBold from "./fonts/721269/aad99a1f-7917-4dd6-bbb5-b07cedbff64f.woff2";
+
 const GlobalsStyles = createGlobalStyle`
   /* Global Reset */
   /*! modern-normalize | MIT License | https://github.com/sindresorhus/modern-normalize */
   html{box-sizing:border-box}*,::after,::before{box-sizing:inherit}:root{-moz-tab-size:4;tab-size:4}html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}body{font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'}hr{height:0}abbr[title]{text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:SFMono-Regular,Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{padding:.35em .75em .625em}legend{padding:0}progress{vertical-align:baseline}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}
+
+  /* Avenir Fonts */
+  /*
+    This CSS resource incorporates links to font software which is the valuable copyrighted property of Monotype and/or its suppliers. You may not attempt to copy, install, redistribute, convert, modify or reverse engineer this font software. Please contact Monotype with any questions regarding Web Fonts: http://www.fonts.com
+  */
+
+  /* @import url("https://fast.fonts.net/lt/1.css?apiType=css&c=1101c604-2635-449c-96d3-e8592bcc8b44&fontids=721263,721269,721275"); */
+
+  /* @font-face {
+    font-family: "AvenirNextLTW01";
+    font-display: swap;
+    font-style: normal;
+    src: url("${AvenirRegular}")
+      format("woff2");
+  }
+
+  @font-face {
+    font-family: "AvenirNextLTW01";
+    font-display: swap;
+    font-weight: 500;
+    src: url("${AvenirMedium}")
+      format("woff2");
+  }
+
+  @font-face {
+    font-family: "AvenirNextLTW01";
+    font-display: swap;
+    font-weight: 600;
+    src: url("${AvenirDemiBold}")
+      format("woff2");
+  } */
 
   /* Global CSS Variables */
   :root {
@@ -26,6 +61,10 @@ const GlobalsStyles = createGlobalStyle`
 `;
 
 export const AxisTheme = props => {
+  console.log(AvenirRegular
+    ,AvenirMedium
+    ,AvenirDemiBold)
+
   return (
     <Grommet full={true} theme={props.theme || axisThemeConfig}>
       <GlobalsStyles />
