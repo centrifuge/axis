50951e8d5e3e5dbd18ec83c561394253402a5699
Jeff Reiner <theundeadghost@gmail.com>
2019-02-25 15:22:06 +0100

    :sparkles: adding Website button component and reset w/ vars

--- packages/theme/src/AxisTheme.js
+++ packages/theme/src/AxisTheme.js
@@ -5,39 +5,17 @@ import { Grommet, ThemeContext } from "grommet";
 import { axisThemeConfig } from "./theme";
 
 const GlobalsStyles = createGlobalStyle`
-  * {
-    
-    &::-webkit-scrollbar {
-      width: 8px;
-    }
-     
-    &::-webkit-scrollbar-thumb {
-      background: ${axisThemeConfig.global.colors["light-6"]};
-      border-radius: 20px;
-    }
-    
-    &::-webkit-scrollbar-track {
-      background: transparent;
-      border-radius: 20px;
-    }
-   
-  }
-  
-   body,html {
-    margin:0;
-    height:100%;
-    color: ${axisThemeConfig.global.colors["light-1"]};
-    
-  }
-  a {
-    color: ${axisThemeConfig.global.colors.brand};
-    text-decoration: none;
-    &:hover {
-        text-decoration: underline;
-    }
-  }
-  #app-root {
-    height:100%;
+  /* Global Reset */
+  /*! modern-normalize | MIT License | https://github.com/sindresorhus/modern-normalize */
+  html{box-sizing:border-box}*,::after,::before{box-sizing:inherit}:root{-moz-tab-size:4;tab-size:4}html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}body{font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'}hr{height:0}abbr[title]{text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:SFMono-Regular,Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{padding:.35em .75em .625em}legend{padding:0}progress{vertical-align:baseline}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}
+
+  /* Global CSS Variables */
+  :root {
+    --fw-regular: 400;
+    --fw-medium: 500;
+    --fw-demibold: 600;
+
+    --f-stack: AvenirNextLTW01, 'Avenir Next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
   }
 `;
 
