519815c4aca2ff8693aa7e4cc4494de0a2bc5191
Jeff Reiner <theundeadghost@gmail.com>
2019-02-26 09:10:28 +0100

    :sparkles: adding back header and paragprag original sizes

--- .storybook/src/theme/typography.js
+++ .storybook/src/theme/typography.js
@@ -3,8 +3,12 @@ import { storiesOf } from "@storybook/react";
 import { AxisTheme } from "../../../packages/theme/src/AxisTheme";
 import { Box, Heading, Paragraph } from "grommet";
 
+const headingSizes = ["xlarge", "large", "medium", "small"];
+
+const paragraphSizes = ["xxlarge", "xlarge", "large", "medium", "small"];
+
 const Headings = () => (
-  <AxisTheme full={false}>
+  <AxisTheme>
     <>
       <Heading level={1} lined>
         Heading 1
@@ -28,14 +32,42 @@ storiesOf("Axis Theme | Typography /", module).add("Documentation", () => (
   </>
 ));
 
-storiesOf("Axis Theme | Typography / Headings", module).add(
-  "Default",
-  Headings
-);
+storiesOf("Axis Theme | Typography / Headings", module)
+  .add("Default", Headings)
+  .add("Sizes", () => (
+    <AxisTheme>
+      <Box direction={"columns"} gap={"large"}>
+        {headingSizes.map(item => {
+          return (
+            <div key={item}>
+              <Heading level={1} size={item}>
+                {item} H1
+              </Heading>
+              <Heading level={2} size={item}>
+                {item} H2
+              </Heading>
+              <Heading level={3} size={item}>
+                {item} H3
+              </Heading>
+              <Heading level={4} size={item}>
+                {item} H4
+              </Heading>
+              <Heading level={5} size={item}>
+                {item} H5
+              </Heading>
+              <Heading level={6} size={item}>
+                {item} H6
+              </Heading>
+            </div>
+          );
+        })}
+      </Box>
+    </AxisTheme>
+  ));
 
 storiesOf("Axis Theme | Typography / Paragraph", module)
   .add("Default", () => (
-    <AxisTheme full={false}>
+    <AxisTheme>
       <>
         <Paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
@@ -53,20 +85,20 @@ storiesOf("Axis Theme | Typography / Paragraph", module)
     </AxisTheme>
   ))
   .add("Sizes", () => (
-    <AxisTheme full={false}>
+    <AxisTheme>
       <Box direction={"columns"} gap={"large"}>
-        {["medium", "large"].map(item => (
+        {paragraphSizes.map(item => (
           <Paragraph key={item} size={item}>
-            <b>{item}:</b> Proin pellentesque, arcu vitae maximus pharetra, diam
-            nulla porttitor orci, gravida aliquam neque nulla et quam. Integer
-            aliquam finibus purus vel vestibulum. Interdum et malesuada fames ac
-            ante ipsum primis in faucibus. Aliquam commodo, sapien ac sodales
-            cursus, metus tortor mollis mi, nec posuere elit tortor sed urna.
-            Praesent id urna eget massa auctor ornare et sed turpis. Ut sagittis
-            ligula enim, vitae blandit libero tincidunt ac. Aenean erat nisl,
-            porttitor at dolor id, laoreet iaculis dolor. Cras tristique
-            convallis lectus ac posuere. Mauris eget diam eu velit tristique
-            blandit.
+            <strong>{item}:</strong> Proin pellentesque, arcu vitae maximus
+            pharetra, diam nulla porttitor orci, gravida aliquam neque nulla et
+            quam. Integer aliquam finibus purus vel vestibulum. Interdum et
+            malesuada fames ac ante ipsum primis in faucibus. Aliquam commodo,
+            sapien ac sodales cursus, metus tortor mollis mi, nec posuere elit
+            tortor sed urna. Praesent id urna eget massa auctor ornare et sed
+            turpis. Ut sagittis ligula enim, vitae blandit libero tincidunt ac.
+            Aenean erat nisl, porttitor at dolor id, laoreet iaculis dolor. Cras
+            tristique convallis lectus ac posuere. Mauris eget diam eu velit
+            tristique blandit.
           </Paragraph>
         ))}
       </Box>
