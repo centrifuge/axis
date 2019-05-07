917e2beec0cfbdeddebe8563af193ad9415ef9c0
razvan <razvan@centrifuge.io>
2019-05-07 11:25:02 +0300

    add opened and headingLevel props

--- packages/modal/src/Modal.js
+++ packages/modal/src/Modal.js
@@ -4,39 +4,42 @@ import { withTheme } from "styled-components";
 
 export const Modal = withTheme(props => {
   const Close = props.theme.modal.icons.close;
+  if (props.opened)
+      return (
+            <Layer
+              position="center"
+              modal
+              onClickOutside={props.onClose}
+              onEsc={props.onClose}
+            >
+              <Box
+                round="small"
+                background="white"
+                elevation="medium"
+                fill="vertical"
+                width={props.size}
+                pad="medium"
+              >
+                <Box
+                  flex={false}
+                  direction="row"
+                  justify={props.title ? "between" : "end"}
+                >
+                  {props.title && (
+                    <Heading level={props.headingLevel || 2} margin="none">
+                      {props.title}
+                    </Heading>
+                  )}
+                  {props.onClose && (
+                    <Button plain label={<Close />} onClick={props.onClose} />
+                  )}
+                </Box>
 
-  return (
-    <Layer
-      position="center"
-      modal
-      onClickOutside={props.onClose}
-      onEsc={props.onClose}
-    >
-      <Box
-        round="small"
-        background="white"
-        elevation="medium"
-        fill="vertical"
-        width={props.size}
-        pad="medium"
-      >
-        <Box
-          flex={false}
-          direction="row"
-          justify={props.title ? "between" : "end"}
-        >
-          {props.title && (
-            <Heading level={2} margin="none">
-              {props.title}
-            </Heading>
-          )}
-          {props.onClose && (
-            <Button plain label={<Close />} onClick={props.onClose} />
-          )}
-        </Box>
+                {props.children}
+              </Box>
+            </Layer>
 
-        {props.children}
-      </Box>
-    </Layer>
-  );
+      );
+
+  return null;
 });
