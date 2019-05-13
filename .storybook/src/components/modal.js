eb1691b23b6a78b5d6c789c2ddf58b71163966a1
razvan <razvan@centrifuge.io>
2019-05-13 14:50:24 +0300

    expose modal heading api

--- .storybook/src/components/modal.js
+++ .storybook/src/components/modal.js
@@ -1,54 +1,91 @@
-import React, { Component } from "react";
-import { storiesOf } from "@storybook/react";
+import React, {Component} from "react";
+import {storiesOf} from "@storybook/react";
 
-import { AxisTheme } from "../../../packages/theme/src/";
-import { Box, Button, Paragraph } from "grommet";
-import { Modal } from "../../../packages/modal/src/";
+import {AxisTheme} from "../../../packages/theme/src/";
+import {Box, Button, Paragraph} from "grommet";
+import {Modal} from "../../../packages/modal/src/";
 
-storiesOf("3.Components | Modal", module).add("default", () => {
-  class ModalContainer extends Component {
-    state = { opened: false };
+storiesOf("3.Components | Modal", module)
+    .add("default", () => {
+        class ModalContainer extends Component {
+            state = {opened: false};
 
-    render() {
-      return (
-        <AxisTheme>
-          <Box fill align="center" justify="center">
-            <Button
-              label={"Open Modal"}
-              onClick={() => {
-                this.setState({ opened: true });
-              }}
-            />
+            render() {
+                return (
+                    <AxisTheme>
+                        <Box fill align="center" justify="center">
+                            <Button
+                                label={"Open Modal"}
+                                onClick={() => {
+                                    this.setState({opened: true});
+                                }}
+                            />
 
-              <Modal
-                opened={this.state.opened}
-                width={"medium"}
-                title={"My First Modal Title"}
-                headingLevel={3}
-                onClose={() => {
-                  this.setState({ opened: false });
-                }}
-              >
-                <Paragraph>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-                  pellentesque, arcu vitae maximus pharetra, diam nulla
-                  porttitor orci, gravida aliquam neque nulla et quam. Integer
-                  aliquam finibus purus vel vestibulum. Interdum et malesuada
-                  fames ac ante ipsum primis in faucibus. Aliquam commodo,
-                  sapien ac sodales cursus, metus tortor mollis mi, nec posuere
-                  elit tortor sed urna. Praesent id urna eget massa auctor
-                  ornare et sed turpis. Ut sagittis ligula enim, vitae blandit
-                  libero tincidunt ac. Aenean erat nisl, porttitor at dolor id,
-                  laoreet iaculis dolor. Cras tristique convallis lectus ac
-                  posuere. Mauris eget diam eu velit tristique blandit.
-                </Paragraph>
-              </Modal>
+                            <Modal
+                                opened={this.state.opened}
+                                width={"medium"}
+                                title={"My First Modal Title"}
+                                onClose={() => {
+                                    this.setState({opened: false});
+                                }}
+                            >
+                                <Paragraph>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                                    pellentesque, arcu vitae maximus pharetra, diam nulla
+                                    porttitor orci, gravida aliquam neque nulla et quam. Integer
+                                    aliquam finibus purus vel vestibulum. Interdum et malesuada
+                                    fames ac ante ipsum primis in faucibus. Aliquam commodo,
+                                    sapien ac sodales cursus, metus tortor mollis mi, nec posuere
+                                    elit tortor sed urna. Praesent id urna eget massa auctor
+                                    ornare et sed turpis. Ut sagittis ligula enim, vitae blandit
+                                    libero tincidunt ac. Aenean erat nisl, porttitor at dolor id,
+                                    laoreet iaculis dolor. Cras tristique convallis lectus ac
+                                    posuere. Mauris eget diam eu velit tristique blandit.
+                                </Paragraph>
+                            </Modal>
 
-          </Box>
-        </AxisTheme>
-      );
-    }
-  }
+                        </Box>
+                    </AxisTheme>
+                );
+            }
+        }
 
-  return <ModalContainer />;
-});
+        return <ModalContainer/>;
+    })
+    .add("Custom Heading", () => {
+        class ModalContainer extends Component {
+            state = {opened: false};
+
+            render() {
+                return (
+                    <AxisTheme>
+                        <Box fill align="center" justify="center">
+                            <Button
+                                label={"Open Modal"}
+                                onClick={() => {
+                                    this.setState({opened: true});
+                                }}
+                            />
+
+                            <Modal
+                                opened={this.state.opened}
+                                width={"medium"}
+                                title={"My First Modal Title"}
+                                headingProps={{level: 3,color:"status-error"}}
+                                onClose={() => {
+                                    this.setState({opened: false});
+                                }}
+                            >
+                                <Paragraph>
+                                    Lorem ipsum
+                                </Paragraph>
+                            </Modal>
+
+                        </Box>
+                    </AxisTheme>
+                );
+            }
+        }
+
+        return <ModalContainer/>;
+    })
