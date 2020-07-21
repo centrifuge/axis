8c3787478aa1c006b5df0b514e709ae0e079763f
Jeroen Offerijns <jeroen@offerijns.com>
2020-07-21 16:25:01 +0200

    feat: update modal design, add title icon to modal

--- packages/modal/src/Modal.tsx
+++ packages/modal/src/Modal.tsx
@@ -1,8 +1,30 @@
 import React, { FunctionComponent } from 'react'
+import styled from 'styled-components'
 import { Box, BoxProps, Button, Heading, HeadingProps, Layer } from 'grommet'
 import { withTheme, ThemeProps as StyledThemeProps } from 'styled-components'
 import { Close, Icon } from 'grommet-icons'
 import { defaultProps, extendDefaultTheme } from 'grommet/default-props'
+
+const StyledTitleIcon = styled.div`
+  display: inline-block;
+  margin-right: 10px;
+  position: relative;
+  top: 4px;
+
+  > * {
+    height: 22px;
+    width: 22px;
+  }
+`
+
+const Title = styled.div`
+  display: inline-block;
+`
+
+const CloseButton = styled(Button)`
+  margin-right: -20px;
+  margin-top: -60px;
+`
 
 interface ThemeProps {
   modal: {
@@ -16,27 +38,46 @@ interface Props extends BoxProps, StyledThemeProps<ThemeProps> {
   headingProps?: HeadingProps
   title?: string
   opened?: boolean
+  titleIcon?: React.ReactNode
   onClose?: () => {}
 }
 
-export const Modal: FunctionComponent<Props> = ({ headingProps, onClose, opened, children, title, theme, ...rest }) => {
+export const Modal: FunctionComponent<Props> = ({
+  headingProps,
+  onClose,
+  opened,
+  children,
+  title,
+  titleIcon,
+  theme,
+  ...rest
+}) => {
   const CloseIcon = theme.modal.icons.close
 
   if (opened)
     return (
       <Layer position="center" modal onClickOutside={onClose} onEsc={onClose}>
-        <Box round="small" background={'white'} pad="medium" {...rest}>
+        <Box round="small" background={'white'} pad="large" {...rest}>
           <Box flex={false} direction="row" alignContent="center" justify={title ? 'between' : 'end'}>
             {title && (
               <Heading
-                level={2}
-                margin={{ top: 'none', bottom: 'none', left: 'none', right: 'medium' }}
+                level={4}
+                margin={{ top: 'none', bottom: 'small', left: 'none', right: 'medium' }}
                 {...headingProps}
               >
-                {title}
+                {titleIcon && <StyledTitleIcon>{titleIcon}</StyledTitleIcon>}
+                <Title>{title}</Title>
               </Heading>
             )}
-            {onClose && <Button plain label={<CloseIcon />} onClick={onClose} />}
+            {onClose && (
+              <CloseButton
+                plain
+                label={
+                  <CloseIcon style={{ width: '16px', height: '16px', marginRight: '-20px', marginLeft: '-20px' }} />
+                }
+                onClick={onClose}
+              />
+            )}
           </Box>
 
           {children}
