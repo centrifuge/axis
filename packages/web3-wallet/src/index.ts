dae0dfd428d45c87da1605da2fdb6a6c2d6f6eb6
Jeroen Offerijns <jeroen@offerijns.com>
2020-07-28 16:36:19 +0200

    fix: missing rename

--- packages/web3-wallet/src/index.ts
+++ packages/web3-wallet/src/index.ts
@@ -3,6 +3,6 @@ import Web3Wallet from './Web3Wallet'
 // Exporting imported interface isn't possible by default, since Typescript doesn't know whether it's a type or a value,
 // so the workaround is to explicitly export it as a type.
 import * as Types from './types'
-export type ToastData = Types.ToastData
+export type Transaction = Types.Transaction
 
 export { Web3Wallet }
