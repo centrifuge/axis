4b4ad9d6fb0a8c6b8af5dcce254b24d74c119cd4
Jeroen Offerijns <jeroen@offerijns.com>
2020-07-29 11:46:08 +0200

    fix: border radius of toasts

--- packages/web3-wallet/src/styles.ts
+++ packages/web3-wallet/src/styles.ts
@@ -11,7 +11,7 @@ export const ToastCard = styled.div<ToastCardProps>`
   height: 60px;
   background: ${props => props.backgroundColor || '#fff'};
   box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.3);
-  border-radius: 4px;
+  border-radius: 8px;
   display: flex;
   flex-direction: row;
 `
