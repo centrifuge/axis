962ebfc330fd4064ebe9547c295d03ae4339195a
Jeroen Offerijns <jeroen@offerijns.com>
2020-08-11 18:14:33 +0200

    chore: format

--- packages/ratio-bar/src/styles.ts
+++ packages/ratio-bar/src/styles.ts
@@ -3,51 +3,51 @@ import styled from 'styled-components'
 import { Segment } from './types'
 
 export const Wrapper = styled.div`
-    display: flex;
+  display: flex;
 `
 
 export const Label = styled.div`
-    flex: 0 0 60px;
-    font-size: 16px;
-    font-weight: bold;
-    display: flex;
-    align-items: center;
+  flex: 0 0 60px;
+  font-size: 16px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
 `
 
 export const Bar = styled.div`
-    flex-grow: 1;
-    margin-right: 12px;
+  flex-grow: 1;
+  margin-right: 12px;
 `
 
 export const InnerBar = styled.div``
 
 interface SegmentDivProps extends Segment {
-    isFirst?: boolean
-    isLast?: boolean
+  isFirst?: boolean
+  isLast?: boolean
 }
 
 export const SegmentDiv = styled.div<SegmentDivProps>`
-    display: inline-block;
-    transition: width 200ms linear;
-    height: 32px;
-    width: ${props => props.separatorText ?  `calc(${props.width}% - 1px)` : `${props.width}%`};
-    background: ${props => props.backgroundColor};
-    border-top-left-radius: ${props => props.isFirst ? '8px' : 0};
-    border-bottom-left-radius: ${props => props.isFirst ? '8px' : 0};
-    border-top-right-radius: ${props => props.isLast ? '8px' : 0};
-    border-bottom-right-radius: ${props => props.isLast ? '8px' : 0};
-    margin-right: -1px;
+  display: inline-block;
+  transition: width 200ms linear;
+  height: 32px;
+  width: ${props => (props.separatorText ? `calc(${props.width}% - 1px)` : `${props.width}%`)};
+  background: ${props => props.backgroundColor};
+  border-top-left-radius: ${props => (props.isFirst ? '8px' : 0)};
+  border-bottom-left-radius: ${props => (props.isFirst ? '8px' : 0)};
+  border-top-right-radius: ${props => (props.isLast ? '8px' : 0)};
+  border-bottom-right-radius: ${props => (props.isLast ? '8px' : 0)};
+  margin-right: -1px;
 `
 
 interface SeparatorProps extends Segment {}
 
 export const Separator = styled.div<SeparatorProps>`
-    width: 1px;
-    height: 40px;
-    background-color: ${props => props.separatorColor || '#000'};
-    display: inline-block;
-    position: relative;
-    top: ${props => props.separatorPosition === 'bottom' ? '8px' : '0'};
+  width: 1px;
+  height: 40px;
+  background-color: ${props => props.separatorColor || '#000'};
+  display: inline-block;
+  position: relative;
+  top: ${props => (props.separatorPosition === 'bottom' ? '8px' : '0')};
 `
 
 export const TopSeparators = styled.div``
@@ -58,11 +58,11 @@ interface SeparatorTextProps extends Segment {}
 
 // margin-left is [center of the separator] - [half number of characters in text * average character width]
 export const SeparatorText = styled.div<SeparatorTextProps>`
-    display: inline;
-    color: ${props => props.separatorColor || '#000'};
-    transition: margin-left 200ms linear;
-    margin-left: ${props =>
+  display: inline;
+  color: ${props => props.separatorColor || '#000'};
+  transition: margin-left 200ms linear;
+  margin-left: ${props =>
     props.widthSoFar ? `calc(${props.widthSoFar}% - ${(props.separatorText || []).length / 2}ch)` : '0'};
-    overflow: hidden;
-    white-space: nowrap;
+  overflow: hidden;
+  white-space: nowrap;
 `
